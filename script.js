41eba06672e77eb3d49e5da8259354d1bd8d6de9
ShodriiBs <rodri.bustos02@gmail.com>
2024-07-26 21:14:04 -0300

    Corrección en las declaraciones de variables y se modificó el array de arrays por un array de objetos

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var ingresoConfirm = confirm("¡Bienvenid@ a la concesionaria Bustos!")
-var arrayInfoAutos = []
+let ingresoConfirm = confirm("¡Bienvenid@ a la concesionaria Bustos!")
+let arrayInfoAutos = []
 
 if (!ingresoConfirm) {
     console.log("No quiso ser atendid@ jaja")
 } else {
     while (ingresoConfirm) {
-        var numMenu = menuPrincipal()
+        const numMenu = menuPrincipal()
+        console.log(numMenu)
 
         if (numMenu == 7) {
+            salirConcesionaria()
             ingresoConfirm = false
         } else {
-            var seguirComprando = confirm("¿Desea seguir viendo otros vehículos? \nAceptar = Sí \nCancelar = No")
+            const seguirComprando = confirm("¿Desea seguir viendo otros vehículos? \nAceptar = Sí \nCancelar = No")
             if (!seguirComprando) {
                 salirConcesionaria()
                 ingresoConfirm = false
@@ -19,128 +21,117 @@ if (!ingresoConfirm) {
     }
 }
 
-function menuPrincipal() { //Menú principal de la concesionaria
-    var grupoAutos = parseInt(prompt("Seleccione qué marca de auto está interesad@: \n1- Volkswagen \n2- Peugeot \n3- Nissan \n4- Fiat \n5- Renault \n6- Audi \n7- Salir"))
+function menuPrincipal() { 
+    const grupoAutos = parseInt(prompt("Seleccione qué marca de auto está interesad@: \n1- Volkswagen \n2- Peugeot \n3- Nissan \n4- Fiat \n5- Renault \n6- Audi \n7- Salir"))
 
     validarOpcionesMenuPrincipal(grupoAutos)
 
     return grupoAutos
 }
 
-function validarOpcionesMenuPrincipal(opcionSeleccionadaMenuPrincipal) { //Valida opción escrita en menú principal
-    var arrayAutosMarcas = ["Volkswagen", "Peugeot", "Nissan", "Fiat", "Renault", "Audi"]
+function validarOpcionesMenuPrincipal(opcionSeleccionadaMenuPrincipal) { 
+    const arrayAutosMarcas = ["Volkswagen", "Peugeot", "Nissan", "Fiat", "Renault", "Audi"]
 
     if (opcionSeleccionadaMenuPrincipal >= 1 && opcionSeleccionadaMenuPrincipal <= 6) {
-        var autosDisponibles = confirm("Autos " + arrayAutosMarcas[opcionSeleccionadaMenuPrincipal - 1] + " disponibles: \n" + autosDisposicion(opcionSeleccionadaMenuPrincipal) + "\n¿Le interesa y/o desea ver la información de los autos mencionados?")
+        const autosDisponibles = confirm("Autos " + arrayAutosMarcas[opcionSeleccionadaMenuPrincipal - 1] + " disponibles: \n" + autosDisposicion(opcionSeleccionadaMenuPrincipal) + "\n¿Le interesa y/o desea ver la información de los autos mencionados?")
         if (autosDisponibles) {
-            var continuarCompra = infoAutosDisponibles(arrayInfoAutos)
+            const continuarCompra = infoAutosDisponibles(arrayInfoAutos)
             if (continuarCompra) {
                 adquirirAuto(arrayInfoAutos, arrayAutosMarcas[opcionSeleccionadaMenuPrincipal - 1])
             }
         } else {
             console.log(autosDisponibles)
-            alert("Redirigiendo al menú principal")
-            menuPrincipal()
         }
     } else if (opcionSeleccionadaMenuPrincipal == 7) {
-        salirConcesionaria()
+        // Salir del menú
     } else {
         opcionNoValida()
         menuPrincipal()
     }
 }
 
-function opcionNoValida() { //Para cuando se escriba una opcion fuera del menú
+function opcionNoValida() { 
     alert("Por favor, seleccione una de las opciones ofrecidas")
     console.log("Se seleccionó opción fuera del menú")
 }
 
-function salirConcesionaria() { //Salir 
+function salirConcesionaria() { 
     alert("¡Muchas gracias! Vuelva pronto")
     console.log("Se cierran los cuadros de dialogos / termina visita a la concesionaria")
 }
 
-function infoAutosDisponibles(allInfoAutoMarca) { //Info de los autos según marca
-    var infoAuto = ""
-    for (var i = 0; i < allInfoAutoMarca.length; i++) {
-        infoAuto += "Modelo: " + allInfoAutoMarca[i][0] + "\nValor USD: " + allInfoAutoMarca[i][1] + "\nAño: " + allInfoAutoMarca[i][2] + "\nVersión: " + allInfoAutoMarca[i][3] + "\nColor: " + allInfoAutoMarca[i][4] + "\n-------------\n"
+function infoAutosDisponibles(allInfoAutoMarca) { 
+    let infoAuto = ""
+    for (let i = 0; i < allInfoAutoMarca.length; i++) {
+        infoAuto = infoAuto + "Modelo: " + allInfoAutoMarca[i].modelo + "\nValor USD: " + allInfoAutoMarca[i].precio + "\nAño: " + allInfoAutoMarca[i].año + "\nVersión: " + allInfoAutoMarca[i].version + "\nColor: " + allInfoAutoMarca[i].color + "\n-------------\n"
     }
     alert(infoAuto)
-    var adquisicionAuto = confirm("¿Está interesad@ en adquirir alguno de estos vehículos? \nAceptar = Sí \nCancelar = No")
+    const adquisicionAuto = confirm("¿Está interesad@ en adquirir alguno de estos vehículos? \nAceptar = Sí \nCancelar = No")
     return adquisicionAuto
 }
 
-function adquirirAuto(autoParaAdquirir, marcaAutoComprar) { //Menu para comprar autos según marca
-    var adquirirAutoValor = ""
-    for (var i = 0; i < autoParaAdquirir.length; i++) {
-        adquirirAutoValor += (i + 1) + "- " + autoParaAdquirir[i][0] + " - USD: " + autoParaAdquirir[i][1] + "\n"
+function adquirirAuto(autoParaAdquirir, marcaAutoComprar) { 
+    let adquirirAutoValor = ""
+    for (let i = 0; i < autoParaAdquirir.length; i++) {
+        adquirirAutoValor = adquirirAutoValor + (i + 1) + "- " + autoParaAdquirir[i].modelo + " - USD: " + autoParaAdquirir[i].precio + "\n"
     }
 
-    var seleccionAuto = parseInt(prompt("Por favor, indique qué auto desea comprar: \n" + adquirirAutoValor + "4- Salir"))
+    const seleccionAuto = parseInt(prompt("Por favor, indique qué auto desea comprar: \n" + adquirirAutoValor + "4- Salir"))
 
     if (seleccionAuto == 1 || seleccionAuto == 2 || seleccionAuto == 3) {
-        alert("¡Muchas gracias por confiar en nosotros! Y felicidades por su nuevo " + marcaAutoComprar + " " + autoParaAdquirir[seleccionAuto - 1][0])
+        alert("¡Muchas gracias por confiar en nosotros! Y felicidades por su nuevo " + marcaAutoComprar + " " + autoParaAdquirir[seleccionAuto - 1].modelo)
     } else if (seleccionAuto == 4) {
-
+        // Salir de la compra
     } else {
         opcionNoValida()
         adquirirAuto(autoParaAdquirir, marcaAutoComprar)
     }
 }
 
-function autosDisposicion(marcaDeAuto) { //Todos los autos disponibles de la concesionaria
-    var allAutos = []
-
+function autosDisposicion(marcaDeAuto) { 
     switch (marcaDeAuto) {
         case 1:
-            allAutos = ["Bora", "Vento", "Passat"];
             arrayInfoAutos = [
-                ["Bora", "9000", "2011", "Highline 1.8T", "Azul oscuro"],
-                ["Vento", "12500", "2013", "Sportline 2.0TSI", "Negro"],
-                ["Passat", "15000", "2020", "Highline", "Blanco"]
-            ]
-            break;
+                { modelo: "Bora", precio: "9000", año: "2011", version: "Highline 1.8T", color: "Azul oscuro" },
+                { modelo: "Vento", precio: "12500", año: "2013", version: "Sportline 2.0TSI", color: "Negro" },
+                { modelo: "Passat", precio: "15000", año: "2020", version: "Highline", color: "Blanco" } ]
+            break
         case 2:
-            allAutos = ["208", "307", "408"]
             arrayInfoAutos = [
-                ["208", "11500", "2021", "Allure 1.6", "Azul"],
-                ["307", "6000", "2010", "XS 2.0", "Gris"],
-                ["408", "10000", "2017", "Feline 1.6", "Blanco"]
-            ]
-            break;
+                { modelo: "208", precio: "11500", año: "2021", version: "Allure 1.6", color: "Azul" },
+                { modelo: "307", precio: "6000", año: "2010", version: "XS 2.0", color: "Gris" },
+                { modelo: "408", precio: "10000", año: "2017", version: "Feline 1.6", color: "Blanco" } ]
+            break
         case 3:
-            allAutos = ["Sentra", "Versa", "Sentra"]
             arrayInfoAutos = [
-                ["Sentra", "12000", "2018", "SR 1.8", "Plateado"],
-                ["Versa", "8000", "2016", "Sense 1.6", "Blanco"],
-                ["Sentra", "10500", "2021", "Sense 1.6", "Negro"]
-            ]
-            break;
+                { modelo: "Sentra", precio: "12000", año: "2018", version: "SR 1.8", color: "Plateado" },
+                { modelo: "Versa", precio: "8000", año: "2016", version: "Sense 1.6", color: "Blanco" },
+                { modelo: "Sentra", precio: "10500", año: "2021", version: "Sense 1.6", color: "Negro" } ]
+            break
         case 4:
-            allAutos = ["Cronos", "Punto", "Palio"]
             arrayInfoAutos = [
-                ["Cronos", "13000", "2019", "Drive 1.3", "Rojo"],
-                ["Punto", "7000", "2014", "Attractive 1.4", "Azul"],
-                ["Palio", "5000", "2012", "ELX 1.3", "Verde"]
-            ]
-            break;
+                { modelo: "Cronos", precio: "13000", año: "2019", version: "Drive 1.3", color: "Rojo" },
+                { modelo: "Punto", precio: "7000", año: "2014", version: "Attractive 1.4", color: "Azul" },
+                { modelo: "Palio", precio: "5000", año: "2012", version: "ELX 1.3", color: "Verde" } ]
+            break
         case 5:
-            allAutos = ["Clio", "Kangoo", "Logan"]
             arrayInfoAutos = [
-                ["Clio", "4000", "2011", "Expression 1.2", "Negro"],
-                ["Kangoo", "6000", "2015", "Express 1.6", "Blanco"],
-                ["Logan", "9000", "2017", "Dynamique 1.6", "Gris"]
-            ]
-            break;
+                { modelo: "Clio", precio: "4000", año: "2011", version: "Expression 1.2", color: "Negro" },
+                { modelo: "Kangoo", precio: "6000", año: "2015", version: "Express 1.6", color: "Blanco" },
+                { modelo: "Logan", precio: "9000", año: "2017", version: "Dynamique 1.6", color: "Gris" } ]
+            break
         case 6:
-            allAutos = ["A4", "A5", "Q3"]
             arrayInfoAutos = [
-                ["A4", "20000", "2019", "TFSI 2.0", "Negro"],
-                ["A5", "25000", "2020", "Sportback 2.0", "Rojo"],
-                ["Q3", "30000", "2021", "TFSI 1.4", "Blanco"]
-            ]
-            break;
+                { modelo: "A4", precio: "20000", año: "2019", version: "TFSI 2.0", color: "Negro" },
+                { modelo: "A5", precio: "25000", año: "2020", version: "Sportback 2.0", color: "Rojo" },
+                { modelo: "Q3", precio: "30000", año: "2021", version: "TFSI 1.4", color: "Blanco" } ]
+            break
     }
 
-    return allAutos.join(" - ");
+    let modelosAutos = ""
+    for (let i = 0; i < arrayInfoAutos.length; i++) {
+        modelosAutos = modelosAutos + "- " + arrayInfoAutos[i].modelo + "\n"
+    }
+
+    return modelosAutos
 }
