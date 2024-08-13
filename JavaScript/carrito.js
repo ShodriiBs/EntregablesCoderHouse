12641dd1e2afa5c7674e9df6310357dd7676b2a4
ShodriiBs <rodri.bustos02@gmail.com>
2024-08-13 01:37:23 -0300

    Entregable 2 Coderhouse: para entregar

--- JavaScript/carrito.js
+++ JavaScript/carrito.js
@@ -1,24 +1,27 @@
 const infoDetallesPedido = document.getElementById("infoCarritoPedidoDetalles");
 const subtotalPedido = document.getElementById("subtotalDiv");
-let infoJSONPedido = localStorage.getItem("infoArrayHelados");
-infoJSONPedido = JSON.parse(infoJSONPedido);
+const dataJSON = document.getElementById("datosEnviadosJSON");
+let infoJSONPedidoString = localStorage.getItem("infoArrayHelados");
 
-for (let i = 0; i < 5; i++) {
-    let obtieneStorage = localStorage.getItem("infoStoragePedido" + i);
-    if (obtieneStorage !== null) {
-        const detallesFinales = document.createElement("div");
-        detallesFinales.innerHTML = `<p>${obtieneStorage}</p>`;
-        infoDetallesPedido.appendChild(detallesFinales);
-    }
+infoJSONPedido = JSON.parse(infoJSONPedidoString);
+
+for (let i = 0; i < infoJSONPedido.length; i++) {
+
+    const detallesFinales = document.createElement("div");
+    detallesFinales.innerHTML = `<p>-${infoJSONPedido[i].Cantidad} pote/s de ${infoJSONPedido[i].Peso} = ${infoJSONPedido[i].Cantidad} * ${infoJSONPedido[i].Precio}</p>`;
+    infoDetallesPedido.appendChild(detallesFinales);
+    
 }
 
 let totalPedido = 0; 
 
 for (let i = 0; i < infoJSONPedido.length; i++) { 
-    totalPedido += infoJSONPedido[i].Precio * infoJSONPedido[i].Cantidad; 
+    totalPedido += infoJSONPedido[i].Precio * infoJSONPedido[i].Cantidad 
 }
 
 subtotalPedido.innerText = `El total del pedido es: $${totalPedido}`;
-totalPedido = 0
 
+dataJSON.innerText = `Detalles de tu factura (Información del localStorage): ${infoJSONPedidoString}`
+
+infoJSONPedido.length = 0
 localStorage.clear()
